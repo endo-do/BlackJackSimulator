aa746691f062b3554ce03cc5c6b053f15a11d4ec
endodo <anmelderhuhnx@mailbox.org>
2025-01-25 13:40:13 +0100

    UI

--- UI.py
+++ UI.py
@@ -13,13 +13,49 @@ class UI:
     """
     def __init__(self):
         self.title = """
-        ______  _               _       ___               _      _____  _                    _         _                
-        | ___ \| |             | |     |_  |             | |    /  ___|(_)                  | |       | |               
-        | |_/ /| |  __ _   ___ | | __    | |  __ _   ___ | | __ \ `--.  _  _ __ ___   _   _ | |  __ _ | |_   ___   _ __ 
-        | ___ \| | / _` | / __|| |/ /    | | / _` | / __|| |/ /  `--. \| || '_ ` _ \ | | | || | / _` || __| / _ \ | '__|
-        | |_/ /| || (_| || (__ |   < /\__/ /| (_| || (__ |   <  /\__/ /| || | | | | || |_| || || (_| || |_ | (_) || |   
-        \____/ |_| \__,_| \___||_|\_\\____/  \__,_| \___||_|\_\ \____/ |_||_| |_| |_| \__,_||_| \__,_| \__| \___/ |_|   
+         ____  _            _        _            _    
+        |  _ \| |          | |      | |          | |   
+        | |_) | | __ _  ___| | __   | | __ _  ___| | __
+        |  _ <| |/ _` |/ __| |/ /   | |/ _` |/ __| |/ /
+        | |_) | | (_| | (__|   <  __| | (_| | (__|   < 
+        |____/|_|\__,_|\___|_|\_\|___/ \__,_|\___|_|\_\ 
         """
+        self.border = "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  "
+        self.main_menu_options = "[1] - Freeplay\n[2] - Drill\n[3] - Settings"
+
+    def get_banner(self, left="", middle="", right="", length=19):
+        """
+        Generates a banner with three brackets and automaticly equal padding
+
+        Args:
+            left (str, optional): The title of the left bracket of the banner. Defaults to "".
+            middle (str, optional): The title of the middle bracket. Defaults to "".
+            right (str, optional): The title of the right bracket. Defaults to "".
+            length (int, optional): The total length of each bracket. Defaults to 19.
+
+        Raises:
+            ValueError: Raises error if a title exceeds the maximum bracket length.
+
+        Returns:
+            str: The generated banner.
+        """
+        banner = []
+        
+        for i in (left, middle, right):
+        
+            if len(i) > length:
+                raise ValueError(f"The word '{i}' exceeds the maximum length of 19 chars")
+            
+            padding = length - len(i)
+            right_padding = padding // 2
+            left_padding = padding - right_padding
+            banner.append(f"{" " * left_padding}{i}{" " * right_padding}")
+
+        result = [self.border]
+        result.append(f" |{banner[0]}¦{banner[1]}¦{banner[2]}| ")
+        result.append(self.border)
+        return "\n".join(result)
+
 
     def clear():
         """
--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 
 
 from Path import Path
+from UI import UI as UserInterface
+
+
+PATH = Path()
+UI = UserInterface()
+
 
 if __name__ == "__main__":
-    PATH = Path()
     PATH.initiate()
+
+    while True:
+        UI.title()
+        
